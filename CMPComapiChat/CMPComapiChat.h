66166579920baa82702932b808bc713b6497b572
Dominik Kowalski <d.kowalsky92@gmail.com>
2018-12-05 18:21:29 +0100

    setup pods, created config, client and SDK interface.

---
+++ CMPComapiChat/CMPComapiChat.h
@@ -0,0 +1,19 @@
+//
+//  CMPComapiChat.h
+//  CMPComapiChat
+//
+//  Created by Dominik Kowalski on 05/12/2018.
+//  Copyright © 2018 Donky Networks Limited. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CMPComapiChat.
+FOUNDATION_EXPORT double CMPComapiChatVersionNumber;
+
+//! Project version string for CMPComapiChat.
+FOUNDATION_EXPORT const unsigned char CMPComapiChatVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CMPComapiChat/PublicHeader.h>
+
+
---
+++ Podfile
@@ -0,0 +1,8 @@
+platform :ios, '10.0'
+use_frameworks!
+
+target 'CMPComapiChat' do
+
+pod 'CMPComapiFoundation', :path => '/Users/dominik.kowalski/Documents/comapi-sdk-ios-objc'
+
+end
