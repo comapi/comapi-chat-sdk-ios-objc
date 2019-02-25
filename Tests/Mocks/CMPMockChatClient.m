e0cb3df44be821a9fd2314eed6b6b6ae532f2424
MarcinSwierczek <marcin.swierczek.dev@gmail.com>
2019-02-25 11:22:49 +0100

    test chat store implementation

---
+++ Tests/Mocks/CMPMockChatClient.m
@@ -0,0 +1,71 @@
+//
+// The MIT License (MIT)
+// Copyright (c) 2017 Comapi (trading name of Dynmark International Limited)
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+// documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons
+// to whom the Software is furnished to do so, subject to the following conditions:
+// The above copyright notice and this permission notice shall be included in all copies or substantial portions of the
+// Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT
+// LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+// IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
+
+#import <Foundation/Foundation.h>
+
+#import "CMPComapiChatClient.h"
+
+@interface CMPComapiChatClient ()
+@end
+
+@interface CMPMockChatClient : CMPComapiChatClient
+
+@end
+
+@implementation CMPMockChatClient
+
+- (instancetype)initWithClient:(CMPComapiClient *)client chatConfig:(CMPChatConfig *)chatConfig {
+    self = [super init];
+    return self;
+}
+
+- (CMPChatServices *)services {
+    return nil;
+}
+
+- (void)setPushToken:(NSString *)deviceToken completion:(void(^)(BOOL, NSError * _Nullable))completion NS_SWIFT_NAME(set(pushToken:completion:)) {
+    
+}
+
+- (void)addTypingDelegate:(id<CMPTypingDelegate>)delegate {
+    
+}
+
+- (void)removeTypingDelegate:(id<CMPTypingDelegate>)delegate {
+    
+}
+
+
+- (void)addProfileDelegate:(id<CMPProfileDelegate>)delegate {
+    
+}
+
+- (void)removeProfileDelegate:(id<CMPProfileDelegate>)delegate {
+    
+}
+
+- (void)addParticipantDelegate:(id<CMPParticipantDelegate>)delegate {
+    
+}
+
+- (void)removeParticipantDelegate:(id<CMPParticipantDelegate>)delegate {
+    
+}
+
+@end
+
---
+++ Tests/Mocks/CMPTestStore.m
@@ -0,0 +1,106 @@
+//
+// The MIT License (MIT)
+// Copyright (c) 2017 Comapi (trading name of Dynmark International Limited)
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+// documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons
+// to whom the Software is furnished to do so, subject to the following conditions:
+// The above copyright notice and this permission notice shall be included in all copies or substantial portions of the
+// Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT
+// LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+// IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
+
+#import <Foundation/Foundation.h>
+
+#import "CMPTestStore.h"
+
+@interface CMPTestStore ()
+
+@property (nonatomic, readonly) NSMutableDictionary<NSString *, CMPChatConversation *> *conversations;
+@property (nonatomic, readonly) NSMutableDictionary<NSString *, CMPChatMessage *> *messages;
+@property (nonatomic, readonly) BOOL inTransaction;
+
+@end
+
+@implementation CMPTestStore
+
+- (CMPChatConversation *)getConversation:(NSString *)ID {
+    return [_conversations objectForKey:ID];
+}
+
+- (NSArray<CMPChatConversation *> *)getAllConversations {
+    return [_conversations allValues];
+}
+
+- (BOOL)upsertConversation:(CMPChatConversation *)conversation {
+    [_conversations setValue:conversation forKey:conversation.id];
+    return YES;
+}
+
+- (BOOL)updateConversation:(CMPChatConversation *)conversation {
+    [_conversations setValue:conversation forKey:conversation.id];
+    return YES;
+}
+
+- (BOOL)deleteConversation:(NSString *)ID {
+    [_conversations removeObjectForKey:ID];
+    return YES;
+}
+
+#pragma mark - Messages
+
+- (BOOL)upsertMessage:(CMPChatMessage *)message {
+    [_messages setObject:message forKeyedSubscript:message.id];
+    return YES;
+}
+
+- (BOOL)updateMessageStatus:(CMPChatMessageStatus *)messageStatus {
+    CMPChatMessage *message = [_messages objectForKey:messageStatus.messageID];
+    CMPChatMessageStatus *old =[message.statusUpdates objectForKey:messageStatus.profileID];
+    if ([old messageStatus] < messageStatus.messageStatus) {
+        [message.statusUpdates setValue:messageStatus forKey:messageStatus.profileID];
+    }
+    return YES;
+}
+
+- (BOOL)deleteAllMessages:(NSString *)conversationID {
+    [_messages removeAllObjects];
+    return YES;
+}
+
+- (BOOL)deleteMessage:(NSString *)conversationID messageID:(NSString *)messageID {
+    [_messages objectForKey:messageID];
+    return YES;
+}
+
+#pragma mark - Database operations
+
+- (BOOL)clearDatabase {
+    [_conversations removeAllObjects];
+    [_messages removeAllObjects];
+    return YES;
+}
+
+- (void)beginTransaction {
+    if (!_inTransaction) {
+        _inTransaction = YES;
+    } else {
+        [NSException raise:@"transaction not finished" format:@"transaction not finished", nil];
+    }
+}
+
+- (void)endTransaction {
+    if (_inTransaction) {
+        _inTransaction = NO;
+    } else {
+        [NSException raise:@"transaction not finished" format:@"transaction not finished", nil];
+    }
+}
+
+@end
