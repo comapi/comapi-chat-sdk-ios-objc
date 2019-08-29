84d2f98bc1a5bb0f8332f7ba8bfc885db87074e3
Dominik Kowalski <d.kowalsky92@gmail.com>
2019-08-29 15:17:40 +0200

    Revert "Version 1.0.0 (#22)"
    This reverts commit d7c2b6deeb4ec011b064217eab9bd9b271252293.

--- Scripts/build-test.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-xcodebuild clean analyze -workspace CMPComapiChat.xcworkspace -scheme CMPComapiChat
-xcodebuild build -workspace CMPComapiChat.xcworkspace -scheme CMPComapiChat
-xcodebuild test -workspace CMPComapiChat.xcworkspace -scheme CMPComapiChatTests -destination 'platform=iOS Simulator,name=iPhone SE'
--- Scripts/install.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-pod cache clean --all
-pod install
